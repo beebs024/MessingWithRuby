375a4be40734e8d7d125662fbc1f13ab9f47f929
Matthew Beebe <mattbb24@gmail.com>
2018-04-19 11:41:31 -0400

    Added encrypt and user input

--- Encrypt.rb
+++ Encrypt.rb
@@ -4,10 +4,18 @@ class Encrypt
   "@", "k", "a", "l", "b", "m", "c","n", "d", "o", "e", "p",
   "f", "q", "g", "r", "h", "s", "i", "t", "j", "u", "k",
   "v", "l", "w", "m", "x", "o", "y", "p", "z", " ", " "]
-  encryptedString = "&> i&£g£" #hi there
+  puts "Please insert a single line sting to encrypt:"
+  stringToEncrypt = gets.chomp
+  encryptedString = ""
+  for i in 0..stringToEncrypt.length-1
+    encryptedString += encryptKey.key(stringToEncrypt[i])
+  end
+  puts "The encrypted version of the input is:"
+  puts encryptedString
   decrypted = ""
   for i in 0..encryptedString.length-1
     decrypted+=encryptKey[encryptedString[i]]
   end
+  puts "Decrypted back to its original string"
   puts decrypted
 end
